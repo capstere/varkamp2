b6e448ef8cf33b4a1f7ec4eb621b291e6c93d2f0
capstere <jesper.fredriksson2@gmail.com>
2025-05-07 22:13:23 +0200

    Create bundle.js

---
+++ public/bundle.js
@@ -0,0 +1,9 @@
+// public/bundle.js
+// =================
+// Den här filen *genereras* automatiskt av Rollup (via npm run build).
+// För att uppdatera den, kör i projektroten:
+//    npm run build
+//
+// Innehållet kommer från src/js/*.js och sammanfogas/minifieras här.
+//
+// (Ingen manuell redigering rekommenderas!)
