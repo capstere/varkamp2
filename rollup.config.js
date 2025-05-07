37ff2b2c08634cc283b97b15e1b095ba71c57085
capstere <jesper.fredriksson2@gmail.com>
2025-05-07 22:16:27 +0200

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+// rollup.config.js
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import sass from 'rollup-plugin-sass';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/js/main.js',
+  output: {
+    file: 'public/bundle.js',
+    format: 'iife',
+    name: 'App',
+    sourcemap: true
+  },
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
+      preventAssignment: true
+    }),
+    resolve(),
+    commonjs(),
+    sass({
+      output: 'public/styles.css'
+    }),
+    terser()
+  ]
+};
